37d47681265f2b7dbd46ae993d7b4891563452c5
SangeethaSan21 <83487293+SangeethaSan21@users.noreply.github.com>
2026-03-06 21:57:08 +0530

    Delete src/pages directory

--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import QuizPage from "./quiz/quiz";
-import RoadmapPage from "./roadmap/roadmap";
-import TopicPage from "./input/topic";
-import ProfilePage from "./profile/profile";
-
-export { QuizPage, RoadmapPage, TopicPage, ProfilePage };
